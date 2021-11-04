1246d912ad775e9ae795ffa98b8de33468dc8068
rrjebat <92413505+rrjebat@users.noreply.github.com>
2021-11-04 20:50:27 +0800

    Add files via upload

---
+++ CNN_Train_10s.m
@@ -0,0 +1,54 @@
+function [convnet ,info, X_train, X_val, Y_train, Y_val] = CNN_Train_10s(data_size, X, Y, trainInd, valInd, no_cnn_filter1, no_cnn_filter2, no_cnn_filter3,...
+    size_filter1, size_filter2, size_filter3, no_fc1, no_fc2)
+
+rng(0)
+X_train = X(:,:,:,trainInd);
+X_val = X(:,:,:,valInd);
+Y_train = Y(:,:,:,trainInd);
+Y_val = Y(:,:,:,valInd);
+
+layers = [
+    imageInputLayer([2 data_size])
+    
+    convolution2dLayer([2 size_filter1],no_cnn_filter1,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    convolution2dLayer([2 size_filter2],no_cnn_filter2,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+%     maxPooling2dLayer([1 2],'stride',[1 2])
+    
+    convolution2dLayer([2 size_filter3],no_cnn_filter3,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+%     maxPooling2dLayer([2 2],'stride',2)
+    
+    dropoutLayer(0.2)
+    fullyConnectedLayer(no_fc1)
+    batchNormalizationLayer
+    reluLayer
+    fullyConnectedLayer(no_fc2)
+    batchNormalizationLayer
+    reluLayer
+    fullyConnectedLayer(1)
+    regressionLayer];
+
+miniBatchSize  = 50;
+validationFrequency = floor(numel(Y_train)/miniBatchSize);
+options = trainingOptions('adam', ...
+    'MaxEpochs',200, ...
+    'InitialLearnRate',1e-3, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.1, ...
+    'LearnRateDropPeriod',40, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',{X_val,Y_val}, ...
+    'ValidationFrequency',validationFrequency, ...
+    'Verbose',false,...
+    'ExecutionEnvironment','cpu');
+
+[convnet ,info] = trainNetwork(X_train,Y_train,layers,options);
+
